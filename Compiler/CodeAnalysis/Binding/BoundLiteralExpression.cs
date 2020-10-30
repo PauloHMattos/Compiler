fbf57b8ef9700dafa88fac60d6648bb0f85a2f33
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2020-10-30 11:24:59 -0300

    Implementa encadeamento da arvore sintática

---
+++ Compiler/CodeAnalysis/Binding/BoundLiteralExpression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Compiler.CodeAnalysis.Binding
+{
+    internal sealed class BoundLiteralExpression : BoundExpression
+    {
+        public object Value { get; }
+        public override BoundNodeKind Kind => BoundNodeKind.LiteralExpression;
+        public override Type Type => Value.GetType();
+
+        public BoundLiteralExpression(object value)
+        {
+            Value = value;
+        }
+    }
+}
