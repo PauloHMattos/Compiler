5dfc6d792650ec80788bd97971701b8c5382064e
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2020-10-30 23:54:50 -0300

    Adiciona teste para verificar precedencia de operadores binários

---
+++ Compiler.Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using Compiler.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace Compiler.Tests.CodeAnalysis.Syntax
+{
+    public class ParserTests
+    {
+        [Theory]
+        [MemberData(nameof(GetBinaryOperatorsPairsData))]
+        public void Parser_BinaryExpression_HonorsPrecedences(SyntaxKind op1, SyntaxKind op2)
+        {
+            var op1Precedence = op1.GetBinaryOperatorPrecedence();
+            var op2Precedence = op2.GetBinaryOperatorPrecedence();
+            var op1Text= op1.GetText();
+            var op2Text = op2.GetText();
+
+            var text = $"a {op1Text} b {op2Text} c";
+            var expression = SyntaxTree.Parse(text).Root;
+
+            if (op1Precedence >= op2Precedence)
+            {
+                //       op2
+                //      /  \
+                //    op1   c
+                //   /  \
+                //  a    b
+                using (var e = new AssertingEnumerator(expression))
+                {
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "a");
+                    e.AssertToken(op1, op1Text);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "b");
+                    e.AssertToken(op2, op2Text);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "c");
+                }
+            }
+            else
+            {
+                //    op1
+                //   /  \
+                //  a   op2
+                //     /  \
+                //    b    c
+                using (var e = new AssertingEnumerator(expression))
+                {
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "a");
+                    e.AssertToken(op1, op1Text);
+                    e.AssertNode(SyntaxKind.BinaryExpression);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "b");
+                    e.AssertToken(op2, op2Text);
+                    e.AssertNode(SyntaxKind.NameExpression);
+                    e.AssertToken(SyntaxKind.IdentifierToken, "c");
+                }
+            } 
+        }
+
+        public static IEnumerable<object[]> GetBinaryOperatorsPairsData()
+        {
+            foreach (var op1 in SyntaxFacts.GetBinaryOperatorKinds())
+            {
+                foreach (var op2 in SyntaxFacts.GetBinaryOperatorKinds())
+                {
+                    yield return new object[] { op1, op2 };
+                }
+            }
+        }
+    }
+}
