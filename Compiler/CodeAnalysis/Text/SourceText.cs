d8657a93bcb420ea02b4f98d4874e666cf8c4d9e
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2020-11-01 10:25:15 -0300

    Corrige SourceText.ParseLines não incluir a ultima linha se ela for vazia

--- Compiler/CodeAnalysis/Text/SourceText.cs
+++ Compiler/CodeAnalysis/Text/SourceText.cs
@@ -71,7 +71,7 @@ namespace Compiler.CodeAnalysis.Text
                 }
             }
 
-            if (position > lineStart)
+            if (position >= lineStart)
             {
                 AddLine(result, sourceText, position, lineStart, 0);
             }
