57b1a6140d32e6aef471cda21a765fc5a887e71f
Paulo Mattos <paulo.mattos@poli.ufrj.br>
2020-11-05 15:34:34 -0300

    Corrige erro no repl em ultima linha no console

--- REPL/Repl.cs
+++ REPL/Repl.cs
@@ -70,7 +70,7 @@ namespace Compiler.REPL
         {
             private readonly Action<string> _lineRenderer;
             private readonly ObservableCollection<string> _submissionDocument;
-            private readonly int _cursorTop;
+            private int _cursorTop;
             private int _renderedLineCount;
             private int _currentLine;
             private int _currentCharacter;
@@ -97,6 +97,16 @@ namespace Compiler.REPL
 
                 foreach (var line in _submissionDocument)
                 {
+                    if (_cursorTop + lineCount >= Console.WindowHeight)
+                    {
+                        Console.SetCursorPosition(0, Console.WindowHeight - 1);
+                        Console.WriteLine();
+                        if (_cursorTop > 0)
+                        {
+                            _cursorTop--;
+                        }
+                    }
+
                     Console.SetCursorPosition(0, _cursorTop + lineCount);
                     Console.ForegroundColor = ConsoleColor.Green;
 
@@ -111,7 +121,7 @@ namespace Compiler.REPL
 
                     Console.ResetColor();
                     _lineRenderer(line);
-                    Console.WriteLine(new string(' ', Console.WindowWidth - line.Length));
+                    Console.Write(new string(' ', Console.WindowWidth - line.Length - 2));
                     lineCount++;
                 }
 
@@ -570,7 +580,7 @@ namespace Compiler.REPL
                         Console.Out.WriteSpace();
                     }
                 }
-                
+
                 Console.Out.WriteSpace();
                 Console.Out.WriteSpace();
                 Console.Out.WriteSpace();
